dbfb3aa6b786b449df74f930fe87af4451b74487
mykolayes <32031506+mykolayes@users.noreply.github.com>
2020-02-16 00:49:44 +0200

    added full project (as deployed in Tomcat folder)

--- DemoServlet/src/DemoServlet.java
+++ DemoServlet/src/DemoServlet.java
@@ -1,8 +1,9 @@
-package net.codejava.servlet;
+package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/demoServlet")
 public class DemoServlet extends HttpServlet {
-
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	                     throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<div align=\"center\">");
+		out.println("<form method=\"post\" action=\"demoServlet\">");
+		out.println("Username: <input type=\"text\" name=\"username\"/> <br/>");
+		out.println("Password: <input type=\"password\" name=\"password\"/> <br/><br/>");
+		out.println("<br/><br/>");
+		out.println("<input type=\"submit\" value=\"Submit\" />");
+		out.println("</form>");
+		out.println("</div");
+		out.println("</body></html>");
+		// RequestDispatcher view = request.getRequestDispatcher("C:/Dev/TomcatDir/webapps/DemoServlet/FormDemo.html");
+		// view.forward(request, response);
+	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("username");
